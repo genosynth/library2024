fb3bae3586315b6adb6c2a54addba8b2ff387f59
genosynth <jpgenovese@gmail.com>
2024-11-05 05:22:23 +0100

    adding books to table and array done

--- library.js
+++ library.js
@@ -49,14 +49,30 @@ function displayBooks(books){
   displayBooks(books)
 
   console.log(title)
+
+  //Table work
+  
+  const table = document.getElementById("table")
+  const tr = document.createElement("tr")
+  const td = document.createElement("td")
+  td.innerText = document.getElementById("book-title").value //Title
+  tr.appendChild(td)
+  const td2 = document.createElement("td")
+  td2.innerText=document.getElementById("book-author").value  //Author
+  tr.appendChild(td2)
+  const td3 = document.createElement("td")
+  td3.innerText=document.getElementById("book-pages").value  //Pages
+  tr.appendChild(td3)
+  const td4 = document.createElement("td")
+  td4.innerText=document.getElementById("answer").value //Read
+  tr.appendChild(td4)
+  table.appendChild(tr)
+
   document.getElementById('book-title').value = ``
   document.getElementById('book-author').value = ``
   document.getElementById('book-pages').value = ``
   document.getElementById('answer').value = ``
 
-  //Table work
-
-  document.getElementById('table')
 
 
  } 
